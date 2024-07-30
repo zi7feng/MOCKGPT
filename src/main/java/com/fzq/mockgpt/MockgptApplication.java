9d5d844227310b33a4813e96f8a6a4fd1cd12916
zfeng <zi7feng7@gmail.com>
2024-07-30 18:44:08 -0400

    init

---
+++ src/main/java/com/fzq/mockgpt/MockgptApplication.java
@@ -0,0 +1,13 @@
+package com.fzq.mockgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockgptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockgptApplication.class, args);
+    }
+
+}
